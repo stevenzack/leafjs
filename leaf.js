7b6fc0af7fb1895cd2042ab0fb4006decaf0f123
Steven Zack <zigzigcheers@gmail.com>
2023-07-03 01:45:32 +0800

    for

--- leaf.js
+++ leaf.js
@@ -1,4 +1,4 @@
-function createValue(value) {
+function createValue(data) {
     return {
         __observers: [],
         postValue: function (v) {
@@ -7,7 +7,12 @@ function createValue(value) {
                 this.__observers[i](v);
             }
         },
-        value: value,
+        notifyUpdate: function () {
+            for (var i = 0; i < this.__observers.length; i++) {
+                this.__observers[i](this.value);
+            }
+        },
+        value: data,
     }
 }
 var __leaf_randomIDs = {}
@@ -30,30 +35,193 @@ function __leaf_generateID(length) {
 }
 
 function renderLeaf(elemID, $) {
-    return __leaf_parse(document.getElementById(elemID), $);
+    $._root = $;
+    return __leaf_hydrate(document.getElementById(elemID), $);
 }
 function __leaf_isLowerCaseEnglish(c) {
     var l = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
     for (var i = 0; i < l.length; i++) {
-        if (c == l.charAt(i)) {
+        if (c === l.charAt(i)) {
             return true;
         }
     }
     return false;
+}
+
+function __leaf_createElemByString(s) {
+    var div = document.createElement('div');
+    div.innerHTML = s;
+    return div.firstChild;
 }
 
 function __leaf_isVariableName(c) {
     var l = 'abcdefghijklmnopqrstuvwxyz';
     for (var i = 0; i < l.length; i++) {
-        if (c == l.charAt(i)) {
+        if (c === l.charAt(i)) {
             return true;
         }
     }
     return false;
 }
 
-function __leaf_parse(elem, $) {
+function __leaf_hydrate(elem, $) {
     var s = elem.innerHTML;
+
+    // attributes
+    for (var i = 0; i < elem.attributes.length; i++) {
+        var name = elem.attributes[i].name;
+        if (!name.startsWith('l-')) {
+            continue;
+        }
+
+        name = name.substring(2);
+        var value = '';
+        if (name.startsWith('style-')) {
+            value = name.substring(6);
+            name = 'style';
+        } else if (name.startsWith('class-')) {
+            value = name.substring(6);
+            name = 'class';
+        } else if (name === 'for') {
+            var token = elem.attributes[i].value;
+            var result = __leaf_executeToken(token, $);
+            if (!result) {
+                throw new Error('the returned type is not an observable<Array>: l-for="' + token + '", instead it is ' + result.value)
+            }
+            if (result.__observers && result.postValue && result.value instanceof Array || result instanceof Array) {
+            } else {
+                throw new Error('the returned type is not an observable or array: l-for="' + token + '"')
+            }
+
+            elem.removeAttribute('l-for');
+            elem.setAttribute('__leaf_for_item', token);
+
+            var fn = function (parentNode, forLoopTemplate, datalist, token) {
+                var domList = [];
+                var started = false;
+                for (var j = 0; j < parentNode.children.length; j++) {
+                    var sibling = parentNode.children[j];
+                    if (sibling.tagName === elem.tagName && sibling.getAttribute('__leaf_for_item') === token) {
+                        sibling.style.display = '';
+                        domList.push(sibling);
+                        started = true;
+                        continue;
+                    }
+                    if (!started) {
+                        continue;
+                    }
+                    break;
+                }
+
+                // compare
+                var toAppend = [];
+                for (var j = 0; j < domList.length || j < datalist.length; j++) {
+                    if (j >= datalist.length) {
+                        //delete
+                        if (j === 0) {
+                            domList[j].style.display = 'none';
+                        } else {
+                            parentNode.removeChild(domList[j]);
+                        }
+                        continue;
+                    }
+                    var newElem = __leaf_createElemByString(forLoopTemplate);
+                    __leaf_hydrate(newElem, datalist[j]);
+                    if (j < domList.length) {
+                        // replace
+                        parentNode.insertBefore(newElem, domList[j]);
+                        parentNode.removeChild(domList[j]);
+                        continue;
+                    }
+                    //append newElem
+                    toAppend.push(newElem);
+                }
+
+                if (toAppend && toAppend.length > 0) {
+                    var suffixIndex = -1;
+                    for (var k = 0; k < parentNode.children.length; k++) {
+                        if (parentNode.children[k].getAttribute('__leaf_for_item') === token) {
+                            suffixIndex = k;
+                            continue;
+                        }
+                        if (suffixIndex > -1) {
+                            break;
+                        }
+                    }
+
+                    if (suffixIndex == -1) {
+                        throw new Error("suffix index of domList is not found: this means leaf.js unexpectedly delete the hidden first element of l-for, now we couldn't find it any more");
+                    }
+                    var next = null;
+                    if (suffixIndex < parentNode.children.length - 1) {
+                        next = parentNode.children[suffixIndex + 1];
+                    }
+                    for (var n = 0; n < toAppend.length; n++) {
+                        if (next) {
+                            parentNode.insertBefore(toAppend[n], next)
+                        } else {
+                            parentNode.appendChild(toAppend[n]);
+                        }
+                    }
+                }
+            }
+
+            // add observers
+            if (result.__observers && result.postValue) {
+                (function (parentNode, forLoopTemplate, observableListValue, token) {
+                    observableListValue.__observers.push(function (v) {
+                        fn(parentNode, forLoopTemplate, observableListValue.value, token);
+                    });
+                })(elem.parentNode, elem.outerHTML, result, token);
+                result = result.value;
+            }
+            fn(elem.parentNode, elem.outerHTML, result, token)
+
+            return $ // l-for handled, no need to render the rest
+        }
+
+        // normal attributes
+        (function (name, token, observables) {
+            var assembleAttributesData = function (name, token) {
+                var result = __leaf_executeToken(token, $);
+                if (name === 'class') {
+                    if (result) {
+                        __leaf_addClass(elem, value);
+                    } else {
+                        __leaf_removeClass(elem, value);
+                    }
+                    return;
+                } else if (name === 'if') {
+                    if (result) {
+                        elem.style.display = '';
+                    } else {
+                        elem.style.display = 'none';
+                    }
+                    return;
+                }
+                if (typeof result === 'boolean') {
+                    if (result) {
+                        elem.setAttribute(name, value)
+                    } else {
+                        elem.removeAttribute(name)
+                    }
+                } else {
+                    elem.setAttribute(name, result)
+                }
+            };
+            for (var j = 0; j < observables.length; j++) {
+                var node = observables[j];
+                if (node.__observers && node.postValue) {
+                    node.__observers.push(function (v) {
+                        assembleAttributesData(name, token);
+                    })
+                }
+            }
+            assembleAttributesData(name, token);
+        })(name, elem.attributes[i].value, __leaf_parseObservablesInToken(elem.attributes[i].value, $))
+    }
+
+
     // top level text
     var childLevel = 0;
     var template = [''];
@@ -64,12 +232,12 @@ function __leaf_parse(elem, $) {
     for (var i = 0; i < s.length - 1; i++) {
         var char1 = s.charAt(i);
         var char2 = s.charAt(i + 1);
-        if (char1 == '<') {
-            if (char2 == '/') {
+        if (char1 === '<') {
+            if (char2 === '/') {
                 var foundEnding = false;
                 i++;
                 for (; i < s.length; i++) {
-                    if (s.charAt(i) == '>') {
+                    if (s.charAt(i) === '>') {
                         foundEnding = true;
                         break;
                     }
@@ -78,7 +246,7 @@ function __leaf_parse(elem, $) {
                     throw new Error('ending arrow for [</] not found: ' + s.substring(0, i));
                 }
                 childLevel--;
-                if (childLevel == 0) {
+                if (childLevel === 0) {
                     template.push('');
                     tokenGroups.push([]);
                 }
@@ -97,14 +265,14 @@ function __leaf_parse(elem, $) {
         }
 
         // top level text
-        if (char1 + char2 == '{{') {
+        if (char1 + char2 === '{{') {
             leftToken = i;
             i++;
             continue;
         }
 
         if (leftToken > -1) {
-            if (char1 + char2 == '}}') {
+            if (char1 + char2 === '}}') {
                 var tokenOrigin = s.substring(leftToken + 2, i);
                 var uniqueID = __leaf_generateID(16);
                 template[template.length - 1] += uniqueID;
@@ -126,6 +294,7 @@ function __leaf_parse(elem, $) {
         }
     }
 
+    // listen innerText
     for (var i = 0; i < tokenGroups.length; i++) {
         var group = tokenGroups[i];
         for (var j = 0; j < group.length; j++) {
@@ -133,73 +302,26 @@ function __leaf_parse(elem, $) {
             var observables = __leaf_parseObservablesInToken(token.origin, $);
             (function (templateIndex) {
                 for (var k = 0; k < observables.length; k++) {
-                    observables[k].__observers.push(function (v) {
-                        __leaf_assembleAndReplaceTopLevelInnerText(elem, template, tokenGroups, templateIndex, $);
-                    });
+                    var node = observables[k];
+                    if (node.__observers && node.postValue) {
+                        node.__observers.push(function (v) {
+                            __leaf_assembleAndReplaceTopLevelInnerText(elem, template, tokenGroups, templateIndex, $);
+                        });
+                    }
                 }
             })(token.templateIndex)
 
         }
     }
-    // attributes
-    for (var i = 0; i < elem.attributes.length; i++) {
-        var name = elem.attributes[i].name;
-        if (name.startsWith('l-')) {
-            name = name.substring(2);
-            var value = '';
-            var observables = __leaf_parseObservablesInToken(elem.attributes[i].value, $);
-            if (name.startsWith('style-')) {
-                value = name.substring(6);
-                name = 'style';
-            } else if (name.startsWith('class-')) {
-                value = name.substring(6);
-                name = 'class';
-            }
-            (function (name, token) {
-                var fn = function (name, token) {
-                    var result = __leaf_executeToken(token, $);
-                    if (name == 'class') {
-                        if (result) {
-                            __leaf_addClass(elem, value);
-                        } else {
-                            __leaf_removeClass(elem, value);
-                        }
-                        return;
-                    } else if (name == 'if') {
-                        if (result) {
-                            elem.style.display = '';
-                        } else {
-                            elem.style.display = 'none';
-                        }
-                        return;
-                    }
-                    if (typeof result == 'boolean') {
-                        if (result) {
-                            elem.setAttribute(name, value)
-                        } else {
-                            elem.removeAttribute(name)
-                        }
-                    } else {
-                        elem.setAttribute(name, result)
-                    }
-                };
-                for (var j = 0; j < observables.length; j++) {
-                    observables[j].__observers.push(function (v) {
-                        fn(name, token);
-                    })
-                }
-                fn(name, token);
-            })(name, elem.attributes[i].value)
-        }
-    }
-    // render initial data
+
+    // render innerText
     for (var i = 0; i < template.length; i++) {
         __leaf_assembleAndReplaceTopLevelInnerText(elem, template, tokenGroups, i, $)
     }
 
     // children
     for (var i = 0; i < elem.children.length; i++) {
-        __leaf_parse(elem.children[i], $);
+        __leaf_hydrate(elem.children[i], $);
     }
     return $;
 }
@@ -217,14 +339,15 @@ function __leaf_addClass(elem, className) {
     if (!s) {
         s = '';
     }
-    if (s.indexOf(className) == -1) {
+    if (s.indexOf(className) === -1) {
         elem.setAttribute('class', s + ' ' + className);
     }
     return;
 }
 
 function __leaf_executeToken(__leaf_token_origin, $) {
-    return eval(__leaf_token_origin);
+    var result = eval(__leaf_token_origin);
+    return result
 }
 
 function __leaf_parseObservablesInToken(tokenOrigin, $) {
@@ -232,20 +355,18 @@ function __leaf_parseObservablesInToken(tokenOrigin, $) {
     for (var i = 0; i < tokenOrigin.length - 1; i++) {
         var char1 = tokenOrigin[i];
         var char2 = tokenOrigin[i + 1];
-        if (char1 + char2 == '$.') {
+        if (char1 + char2 === '$.') {
             i += 2;
             var variableName = '';
             for (; i < tokenOrigin.length; i++) {
                 if (__leaf_isVariableName(tokenOrigin.charAt(i))) {
                     variableName += tokenOrigin.charAt(i);
                 } else {
+                    
                     break;
                 }
             }
             var observable = $[variableName];
-            if (!observable || !observable.__observers || !observable.postValue) {
-                throw new Error('parse template failed: invalid variable name ' + tokenOrigin);
-            }
 
             observables.push(observable);
         }
@@ -254,7 +375,6 @@ function __leaf_parseObservablesInToken(tokenOrigin, $) {
 }
 
 function __leaf_assembleAndReplaceTopLevelInnerText(elem, template, tokenGroups, templateIndex, $) {
-    console.log('assembling: ' + templateIndex);
     var s = elem.innerHTML;
     var childLevel = 0;
     var currentTemplateIndex = 0;
@@ -262,14 +382,14 @@ function __leaf_assembleAndReplaceTopLevelInnerText(elem, template, tokenGroups,
     for (var i = 0; i < s.length - 1; i++) {
         var char1 = s.charAt(i);
         var char2 = s.charAt(i + 1);
-        if (char1 == '<') {
+        if (char1 === '<') {
             builder += char1;
-            if (char2 == '/') {
+            if (char2 === '/') {
                 var foundEnding = false;
                 i++;
                 for (; i < s.length; i++) {
                     builder += s.charAt(i);
-                    if (s.charAt(i) == '>') {
+                    if (s.charAt(i) === '>') {
                         foundEnding = true;
                         break;
                     }
@@ -278,7 +398,7 @@ function __leaf_assembleAndReplaceTopLevelInnerText(elem, template, tokenGroups,
                     throw new Error('ending arrow for [</] not found: ' + s.substring(0, i));
                 }
                 childLevel--;
-                if (childLevel == 0) {
+                if (childLevel === 0) {
                     currentTemplateIndex++;
                 }
                 continue;
@@ -314,7 +434,7 @@ function __leaf_assembleAndReplaceTopLevelInnerText(elem, template, tokenGroups,
         // slipping to current template ending
         for (; i < s.length; i++) {
             var char = s.charAt(i);
-            if (char == '<') {
+            if (char === '<') {
                 i--;
                 break;
             }
